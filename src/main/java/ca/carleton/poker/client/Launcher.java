69a9aa5f9781d9a86a4bb2523b326a0ee42d711c
Michael Hum <michaelhum@cmail.carleton.ca>
2015-09-14 15:34:22 -0400

    Added basic loop in launcher.

--- src/main/java/ca/carleton/poker/client/Launcher.java
+++ src/main/java/ca/carleton/poker/client/Launcher.java
@@ -1,18 +1,15 @@
 package ca.carleton.poker.client;
 
 import ca.carleton.poker.entity.PokerHand;
-import ca.carleton.poker.entity.card.Card;
-import ca.carleton.poker.entity.card.Rank;
-import ca.carleton.poker.entity.card.Suit;
-import ca.carleton.poker.entity.rank.HandRank;
-import ca.carleton.poker.entity.rank.PokerRank;
 import ca.carleton.poker.service.PokerRankService;
 import ca.carleton.poker.service.SimplisticPokerService;
 import ca.carleton.poker.service.input.InputService;
 import ca.carleton.poker.service.input.impl.InputServiceFactory;
 
-import static java.lang.System.out;
+import java.util.ArrayList;
+import java.util.List;
 
+import static java.lang.System.out;
 /**
  * Main class.
  * Created by Mike on 14/09/2015.
@@ -22,21 +19,27 @@ public class Launcher {
     private static final SimplisticPokerService pokerService = new SimplisticPokerService(new PokerRankService());
 
     public static void main(final String[] args) {
-
-        final PokerHand hand1 = new PokerHand();
-        hand1.addCard(new Card(Rank.ONE, Suit.SPADES));
-        hand1.addCard(new Card(Rank.TWO, Suit.SPADES));
-        hand1.addCard(new Card(Rank.THREE, Suit.SPADES));
-        hand1.addCard(new Card(Rank.FOUR, Suit.SPADES));
-        hand1.addCard(new Card(Rank.FIVE, Suit.SPADES));
-        hand1.setPokerRank(new PokerRank(HandRank.STRAIGHT, Rank.FIVE));
-
-        out.println(hand1);
-        out.print("Hand input >>> ");
-
         final InputService in = InputServiceFactory.getInputService();
 
-        final String input = in.getInput();
-        final PokerHand hand = pokerService.makeHand(input);
+        final List<PokerHand> hands = new ArrayList<>(5);
+
+        // TODO split into methods, validation
+        while (true) {
+            out.print("Enter number of players for this round (Q to quit) >>> ");
+            final String input = in.getInput();
+            if (input.equalsIgnoreCase("Q")) {
+                System.exit(0);
+            }
+            final int numberOfPlayers = Integer.parseInt(input);
+            out.println("\nBegin score data (format: playerId RankSuit RankSuit RankSuit RankSuit RankSuit)");
+            for (int i = 1; i <= numberOfPlayers; i++) {
+                out.print("Enter score data for player " + i + " >>>");
+                final String playerInput = in.getInput();
+                final PokerHand playerHand = pokerService.makeHand(playerInput);
+                hands.add(playerHand);
+            }
+            System.out.println(hands);
+        }
+
     }
 }
