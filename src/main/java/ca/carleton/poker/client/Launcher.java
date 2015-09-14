e0eeafc4a70fe9a657b1f010a2db65ff713d933c
Michael Hum <michaelhum@cmail.carleton.ca>
2015-09-14 10:14:07 -0400

    Added example main method shell

--- src/main/java/ca/carleton/poker/client/Launcher.java
+++ src/main/java/ca/carleton/poker/client/Launcher.java
@@ -1,4 +1,4 @@
-package ca.carleton.poker;
+package ca.carleton.poker.client;
 
 import ca.carleton.poker.entity.PokerHand;
 import ca.carleton.poker.entity.card.Card;
@@ -6,12 +6,22 @@ import ca.carleton.poker.entity.card.Rank;
 import ca.carleton.poker.entity.card.Suit;
 import ca.carleton.poker.entity.rank.HandRank;
 import ca.carleton.poker.entity.rank.PokerRank;
+import ca.carleton.poker.service.PokerRankService;
+import ca.carleton.poker.service.SimplisticPokerService;
+
+import java.util.Scanner;
+
+import static java.lang.System.out;
 
 /**
  * Main class.
  * Created by Mike on 14/09/2015.
  */
 public class Launcher {
+
+    private static final Scanner in = new Scanner(System.in);
+
+    private static final SimplisticPokerService pokerService = new SimplisticPokerService(new PokerRankService());
 
     public static void main(final String[] args) {
 
@@ -23,6 +33,9 @@ public class Launcher {
         hand1.addCard(new Card(Rank.FIVE, Suit.SPADES));
         hand1.setPokerRank(new PokerRank(HandRank.STRAIGHT, Rank.FIVE));
 
-        System.out.println(hand1);
+        out.println(hand1);
+        out.print("Hand input >>> ");
+        final String input = in.nextLine();
+        final PokerHand hand = pokerService.makeHand(input);
     }
 }
