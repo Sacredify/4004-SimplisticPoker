deb4f82138ad68e3cca7da0a4adfe7f7eaad8a00
Michael Hum <michaelhum@cmail.carleton.ca>
2015-09-14 08:36:23 -0400

    Added launcher + gitignore.

---
+++ src/main/java/ca/carleton/poker/Launcher.java
@@ -0,0 +1,12 @@
+package ca.carleton.poker;
+
+/**
+ * Main class.
+ * Created by Mike on 14/09/2015.
+ */
+public class Launcher {
+
+    public static void main(final String[] args) {
+        System.out.println("Foo");
+    }
+}
